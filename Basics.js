530d2f445a4e479587773990ab90e6161eb76222
brettl1991 <brettl1991@gmail.com>
2022-09-18 14:21:12 +0100

    JS numbers

---
+++ Basics.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//Numbers
+// Modulo %
+// 27 % 2;//1 because 13*2 and 1 left
+
+//Exponentiation (**) opeartor
+//5**2 equal to 25
+//3 ** 4 equal to 81
+
+//Operations order:
+// Grouping ( () )
+// Not ( ! )
+// Multiplication ( * )
+// Division ( / )
+// Modulus ( % )
+// Addition ( + )
+// Subtraction ( - )
+// Less than ( < )
