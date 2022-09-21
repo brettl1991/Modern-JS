5ecdb5cc85f0de748a36934b3f02cdeffec4a564
brettl1991 <brettl1991@gmail.com>
2022-09-21 11:03:32 +0100

    Learning about variables

--- Basics.js
+++ Basics.js
@@ -17,3 +17,19 @@
 // Addition ( + )
 // Subtraction ( - )
 // Less than ( < )
+
+//Variables: let: can change the value
+let score = 0;
+score += 1;
+let bonusMult = 450;
+score *= bonusMult;
+console.log(score); ////450
+//unary operator: has 1 side
+let counter = 0;
+counter++;
+console.log(counter); //1
+// Variables: const: can not change value
+const year = 1991;
+year += 7; //error
+
+//Variables: old one is var
