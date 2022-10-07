12033bc9ccb7c14222c290625a5d4345027b1775
brettl1991 <brettl1991@gmail.com>
2022-10-07 11:53:58 +0100

    If statement

--- start/app.js
+++ start/app.js
@@ -1,3 +1,11 @@
 "use struct";
 
-alert("Hey, it is working! :)");
+// alert("Hey, it is working! :)");
+
+let num = 37;
+
+if (num % 2 !== 0) {
+  console.log("We've got an odd number");
+} else {
+  console.log("We've got an even number");
+}
