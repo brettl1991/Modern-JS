1b0c97927a1487da66de8c2c213bfcd619be4b53
brettl1991 <brettl1991@gmail.com>
2022-10-07 12:05:40 +0100

    Fix typo

--- start/app.js
+++ start/app.js
@@ -21,7 +21,7 @@ let rating = 2;
 if (rating === 3) {
   console.log("You are a superstar!");
 } else if (rating === 2) {
-  console.log("Meets expectations :");
+  console.log("Meets expectations :)");
 } else if (rating === 1) {
   console.log("Need improvement!");
 }
