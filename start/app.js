0d157981fb8216c07431c26d5b231f09b0656ba8
brettl1991 <brettl1991@gmail.com>
2022-10-07 11:59:02 +0100

    Els if

--- start/app.js
+++ start/app.js
@@ -2,10 +2,24 @@
 
 // alert("Hey, it is working! :)");
 
-let num = 37;
+// let num = 37;
 
-if (num % 2 !== 0) {
-  console.log("We've got an odd number");
-} else {
-  console.log("We've got an even number");
+// if (num % 2 !== 0) {
+//   console.log("We've got an odd number");
+// } else {
+//   console.log("We've got an even number");
+// }
+
+//Performance review
+//3  -  superstar
+//2  -  meets expectations
+//1  -  need improvement
+//anything else  --  wtf?
+
+let rating = 2;
+
+if (rating === 3) {
+  console.log("You are a superstar!");
+} else if (rating === 2) {
+  console.log("Meets expectations :");
 }
