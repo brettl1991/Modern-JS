b158aba5cf213647df47df03e36b19f2874bdbc2
brettl1991 <brettl1991@gmail.com>
2022-10-10 11:55:31 +0100

    Array indices

--- arrays/basics/app.js
+++ arrays/basics/app.js
@@ -4,3 +4,8 @@ let lotto = [45, 12, 23, 25, 34];
 let myCollection = [12, "dog", true, null, NaN];
 
 new Array(1, 2, 3, 4, 5);
+
+console.log(lotto.length);
+console.log(lotto[2]);
+//access last item
+console.log(lotto[lotto.length - 1]); //34
