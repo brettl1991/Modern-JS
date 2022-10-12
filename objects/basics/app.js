49fcd06deff03f3ccb8720b6cc2b81544e24b75c
brettl1991 <brettl1991@gmail.com>
2022-10-12 12:50:58 +0100

    Objects are ref types

--- objects/basics/app.js
+++ objects/basics/app.js
@@ -105,3 +105,18 @@ const game = {
     [null, "O", "X"],
   ],
 };
+
+//Objects and Reference types
+const paletteNew = {
+  red: "#eb4d4b",
+  yellow: "#f9ca24",
+  blue: "#30336b",
+};
+//Objects & Arrays are reference types
+const palette2 = paletteNew;
+//If we change one value...
+palette2.green = "#ebf876";
+
+//Both variables reflect that change...
+palette.green; //"#ebf876"
+palette2.green; //"#ebf876"
