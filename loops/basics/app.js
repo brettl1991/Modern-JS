b37060b11f392a8d7f063e884458ead7658053c7
brettl1991 <brettl1991@gmail.com>
2022-10-12 15:53:07 +0100

    Loops and arrays

--- loops/basics/app.js
+++ loops/basics/app.js
@@ -55,3 +55,73 @@ console.log("AFTER THE LOOP!");
 for (let i = 1; i <= 20; i += 2) {
   console.log(i);
 }
+
+//For loops and arrays
+// ======= EXAMPLE 1 ==========
+// Printing each element in an array
+const examScores = [98, 77, 84, 91, 57, 66];
+
+for (let i = 0; i < examScores.length; i++) {
+  console.log(i, examScores[i]); //[i] to access all individual elements
+}
+// 0 98
+// 1 77
+// 2 84
+// 3 91
+// 4 57
+// 5 66
+
+// ======= EXAMPLE 2 ==========
+// Same idea, but with a more complex array
+const myStudents = [
+  {
+    firstName: "Zeus",
+    grade: 86,
+  },
+  {
+    firstName: "Artemis",
+    grade: 97,
+  },
+  {
+    firstName: "Hera",
+    grade: 72,
+  },
+  {
+    firstName: "Apollo",
+    grade: 90,
+  },
+];
+
+for (let i = 0; i < myStudents.length; i++) {
+  let student = myStudents[i]; //this will give back the individual objects
+  console.log(`${student.firstName} scored ${student.grade}`);
+}
+
+// Zeus scored 86
+// Artemis scored 97
+// Hera scored 72
+// Apollo scored 90
+
+// ======= EXAMPLE 3 ==========
+// Averaging all grades in myStudents array
+let total = 0; //total will hold the sum of all grades
+
+for (let i = 0; i < myStudents.length; i++) {
+  let student = myStudents[i];
+  total += student.grade; //add each grade to total
+}
+console.log(total / myStudents.length); //divide by number of students 86.25
+
+// ======= EXAMPLE 4 ==========
+// Reversing a string
+const word = "stressed";
+
+let reversedWord = ""; //will hold reversed string
+
+//Loop backwards over the string
+for (let i = word.length - 1; i >= 0; i--) {
+  //let i = word.length - 1 means start at d word
+  reversedWord += word[i]; //add each char to reversedWord
+}
+
+console.log(reversedWord); //desserts
