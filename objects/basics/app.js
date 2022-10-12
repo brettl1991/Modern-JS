9520aae9729cf7dc6ae7de3b8ffbff1b81f1353d
brettl1991 <brettl1991@gmail.com>
2022-10-12 12:58:34 +0100

    Objects array equality

--- objects/basics/app.js
+++ objects/basics/app.js
@@ -112,6 +112,7 @@ const paletteNew = {
   yellow: "#f9ca24",
   blue: "#30336b",
 };
+
 //Objects & Arrays are reference types
 const palette2 = paletteNew;
 //If we change one value...
@@ -120,3 +121,29 @@ palette2.green = "#ebf876";
 //Both variables reflect that change...
 palette.green; //"#ebf876"
 palette2.green; //"#ebf876"
+
+//Array-Object equality
+let nums = [1, 2, 3];
+let mystery = [1, 2, 3];
+let moreNums = nums;
+
+//They 'look' the same, but refer to different arrays
+nums === mystery; // false
+
+//These two arrays reference the exact same array, so we get true:
+nums === moreNums; //true
+
+const user = {
+  username: "CherryGarcia8",
+  email: "garcia@gmail.com",
+  notifications: ["message", "alert"],
+};
+
+//THIS WILL NOT WORK!
+if (user.notifications === []) {
+  console.log("NO NEW NOTIFICATIONS!");
+}
+// THIS VERSION DOES WORK!
+if (!user.notifications.length) {
+  console.log("NO NEW NOTIFICATIONS!");
+}
