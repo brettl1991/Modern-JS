54e3c710af1bdb8b65492766f6cb40f2570cad98
brettl1991 <brettl1991@gmail.com>
2022-10-14 11:15:15 +0100

    Nested for loops

--- loops/basics/app.js
+++ loops/basics/app.js
@@ -125,3 +125,47 @@ for (let i = word.length - 1; i >= 0; i--) {
 }
 
 console.log(reversedWord); //desserts
+
+//Nested for loops
+for (let i = 1; i <= 10; i++) {
+  console.log("OUTER LOOP:", i);
+  for (let j = 10; j >= 0; j -= 2) {
+    console.log("  INNER LOOP", j);
+  }
+}
+
+// OUTER LOOP: 1
+//   INNER LOOP 10
+//   INNER LOOP 8
+//   INNER LOOP 6
+//   INNER LOOP 4
+//   INNER LOOP 2
+//   INNER LOOP 0
+// OUTER LOOP: 2
+//   INNER LOOP 10
+//   INNER LOOP 8
+//   INNER LOOP 6
+//   INNER LOOP 4
+//   INNER LOOP 2
+//   INNER LOOP 0... etc
+
+// EXAMPLE 2
+// Sum all elements in our 2048 board
+const gameBoard = [
+  [4, 32, 8, 4],
+  [64, 8, 32, 2],
+  [8, 32, 16, 4],
+  [2, 8, 4, 2],
+];
+
+//sum the values
+let totalScore = 0;
+//outer loop iterates through the rows
+for (let i = 0; i < gameBoard.length; i++) {
+  let row = gameBoard[i]; //access to inner array
+  //inner loop iterates over each cell in a given row
+  for (let j = 0; j < row.length; j++) {
+    totalScore += row[j];
+  }
+}
+console.log(totalScore); //230
