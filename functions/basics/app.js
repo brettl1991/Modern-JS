128e0fd1c8eac91004e7b2ffeccd26701b14c3ab
brettl1991 <brettl1991@gmail.com>
2022-10-17 12:51:47 +0100

    Functions

---
+++ functions/basics/app.js
@@ -0,0 +1,22 @@
+"use strict";
+
+//Function
+// STEP 1: Define the function:
+function grumpus() {
+  console.log("ugh...you again...");
+  console.log("FOR THE LAST TIME...");
+  console.log("LEAVE ME ALONE!!!");
+}
+// STEP 2: Call the function:
+grumpus();
+grumpus();
+grumpus();
+
+for (let i = 0; i < 50; i++) {
+  grumpus();
+}
+
+//50 times this
+// ugh...you again...
+// FOR THE LAST TIME...
+// LEAVE ME ALONE!!!
