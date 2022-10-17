89690b7470bee1eedb88202c89f4aadace9a77e8
brettl1991 <brettl1991@gmail.com>
2022-10-17 13:06:34 +0100

    Arguments

--- functions/basics/app.js
+++ functions/basics/app.js
@@ -52,3 +52,30 @@ throwDice();
 // Rolled: 6
 // Rolled: 2
 // Rolled: 4
+
+//Arguments: function inputs
+// function greet() {
+// 	console.log('Hi');
+// }
+
+// greet now expects a single argument
+function greet(nickname) {
+  console.log(`Hi, ${nickname}!`);
+}
+greet("Sansa");
+greet("Ramsay");
+
+// EXAMPLE 2: modify rolldie from before
+function rollDie() {
+  let roll = Math.floor(Math.random() * 6) + 1;
+  console.log(`Rolled: ${roll}`);
+}
+// We can now specify how many dice to roll!
+function throwDice(numRolls) {
+  for (let i = 0; i < numRolls; i++) {
+    rollDie();
+  }
+}
+
+throwDice(2);
+throwDice(6);
