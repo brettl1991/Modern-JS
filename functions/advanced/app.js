e0ea91ab0537fd75a07ac4a3119419f31f6aa161
brettl1991 <brettl1991@gmail.com>
2022-10-24 13:09:08 +0100

    Callbacks

--- functions/advanced/app.js
+++ functions/advanced/app.js
@@ -238,3 +238,21 @@ isInNineties(1987); //false
 const isNiceWeather = makeBetweenFunc(60, 79);
 isNiceWeather(68); //true
 isNiceWeather(98); //false
+
+//Callbacks: functions to be passed in other function than been invoked
+function grumpus() {
+  alert("GAHHH GO AWAY!");
+}
+
+// setTimeout(callback, delay)
+setTimeout(function () {
+  //we pass an anonymous callback function
+  alert("WELCOME!");
+}, 5000);
+
+//DON'T WORRY ABOUT ANY OF THIS SYNTAX!!
+const btn = document.querySelector("button");
+// JUST FOCUS ON THE CALLBACK WE PASS IN!
+btn.addEventListener("click", function () {
+  alert("WHY DID YOU CLICK ME!!??");
+});
