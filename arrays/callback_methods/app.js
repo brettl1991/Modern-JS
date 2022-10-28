2ae809ea8d6c8efd911b0985b5394cb589120c09
brettl1991 <brettl1991@gmail.com>
2022-10-28 15:46:31 +0100

    Arrow functions

--- arrays/callback_methods/app.js
+++ arrays/callback_methods/app.js
@@ -130,3 +130,29 @@ const titles = booksNew.map(function (b) {
   return b.title;
 });
 //["Good Omens", "Bone: The Complete Edition", "American Gods", "A Gentleman in Moscow"]
+
+// Arrow functions
+//Non-arrow function expression:
+// const square = function (x) {
+//   return x * x;
+// }
+
+// Equivalent arrow function:
+const square = (x) => {
+  return x * x;
+};
+
+//parens are option when you have 1 parameter
+const isEven = (num) => {
+  return num % 2 === 0;
+};
+
+//Must include parens for multiple parameters
+const multiply = (x, y) => {
+  return x * y;
+};
+
+// Must include parens for zero parameters
+const greet = () => {
+  console.log("HI!");
+};
