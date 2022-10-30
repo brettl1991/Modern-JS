80a44744c5d9802fc6aea9b5fa2fe7b7acaa720a
brettl1991 <brettl1991@gmail.com>
2022-10-30 15:47:39 +0000

    Array.find

--- arrays/callback_methods/app.js
+++ arrays/callback_methods/app.js
@@ -193,3 +193,53 @@ const parityList1 = nums.map((n) => {
 const parityList2 = nums.map((n) => (n % 2 === 0 ? "even" : "odd"));
 
 const parityList3 = nums.map((n) => (n % 2 === 0 ? "even" : "odd"));
+
+// Array.find: returns the value of the first element in the array that satisfies the provided function, returns only one element
+let movies = [
+  "The Fantastic Mr. Fox",
+  "Mr. and Mrs. Smith",
+  "Mrs. Doubtfire",
+  "Mr. Deeds",
+];
+
+// Find first movie that includes 'Mrs' in its title:
+const movie = movies.find((movie) => {
+  return movie.includes("Mrs");
+});
+
+//Find first movie that begins with 'Mrs':
+const movie2 = movies.find((m) => m.indexOf("Mrs") === 0);
+
+//Mrs. Doubtfire
+
+const booksNewTwo = [
+  {
+    title: "Good Omens",
+    authors: ["Terry Pratchett", "Neil Gaiman"],
+    rating: 4.25,
+  },
+  {
+    title: "Bone: The Complete Edition",
+    authors: ["Jeff Smith"],
+    rating: 4.42,
+  },
+  {
+    title: "American Gods",
+    authors: ["Neil Gaiman"],
+    rating: 4.11,
+  },
+  {
+    title: "A Gentleman in Moscow",
+    authors: ["Amor Towles"],
+    rating: 4.36,
+  },
+];
+//Find first book w/ rating over 4.3
+const goodBook = booksNewTwo.find((b) => b.rating >= 4.3);
+console.log(goodBook);
+//{title: 'Bone: The Complete Edition', authors: Array(1), rating: 4.42}
+
+//Find first book with Neil Gaiman as an author:
+const neilBook = booksNewTwo.find((b) => b.authors.includes("Neil Gaiman"));
+console.log(neilBook);
+// {title: 'Good Omens', authors: Array(2), rating: 4.25}
