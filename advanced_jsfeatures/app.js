39cefded6b9fbe7b8a60607c3d7a2340861dbed2
brettl1991 <brettl1991@gmail.com>
2022-11-04 11:32:52 +0000

    Array destructuring

--- advanced_jsfeatures/app.js
+++ advanced_jsfeatures/app.js
@@ -207,3 +207,32 @@ fullName("tom", "john", "iii", "order of the phoenix");
 // We can use rest parameters in arrow functions!
 const multiplyNew = (...nums) =>
   nums.reduce((total, currVal) => total * currVal);
+
+//Array destructoring:unpack values from arrays, properties from objects into distinct variable
+const raceResults = [
+  "Eliud Kipchoge",
+  "Feyisa Lelisa",
+  "Galen Rupp",
+  "Ghirmay Ghebreslassie",
+  "Alphonce Simbu",
+  "Jared Ward",
+];
+
+// The old way:
+// const gold = raceResults[0]
+// const silver = raceResults[1]
+// const bronze = raceResults[2]
+
+// Using Destructuring:
+const [gold, silver, bronze] = raceResults;
+gold; //'Eliud Kipchoge'
+silver; //'Feyisa Lelisa'
+bronze; //'Galen Rupp'
+
+const [first, , , fourth] = raceResults; //commas skip elements
+first; //'Eliud Kipchoge'
+fourth; //'Ghirmay Ghebreslassie'
+
+const [winner, ...others] = raceResults;
+winner; //'Eliud Kipchoge'
+others; //["Feyisa Lelisa", "Galen Rupp", "Ghirmay Ghebreslassie", "Alphonce Simbu", "Jared Ward"]
