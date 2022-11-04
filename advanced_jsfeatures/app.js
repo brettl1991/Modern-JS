b449534432cd4bea2348c8f38cc7cce32245f046
brettl1991 <brettl1991@gmail.com>
2022-11-04 12:45:18 +0000

    Parameters destructuring

--- advanced_jsfeatures/app.js
+++ advanced_jsfeatures/app.js
@@ -256,3 +256,64 @@ const { country: nation, title: honorific } = runner;
 
 //using rest operator
 const { firstt, last, ...other } = runner;
+
+// Nested destructuring
+const results = [
+  {
+    first: "Eliud",
+    last: "Kipchoge",
+    country: "Kenya",
+  },
+  {
+    first: "Feyisa",
+    last: "Lilesa",
+    country: "Ethiopia",
+  },
+  {
+    first: "Galen",
+    last: "Rupp",
+    country: "United States",
+  },
+];
+
+//add different var name and get first, than country from 2nd
+// const [{ first: goldWinner }, { country }] = results;
+// goldWinner; //"Eliud"
+// country; //"Ethiopia"
+
+//access to ethiopia in diff way
+const [, silverMedal] = results;
+const { country } = silverMedal;
+console.log(country); //Ethiopia
+
+//Destructuring parameters
+const runnerNew = {
+  first: "Eliud",
+  last: "Kipchoge",
+  country: "Kenya",
+  title: "Elder of the Order of the Golden Heart of Kenya",
+};
+
+// Rather than destructuring INSIDE the function body
+// function print(person) {
+//   const {
+//     first,
+//     last,
+//     title
+//   } = person;
+//   console.log(`${first} ${last}, ${title}`)
+// }
+
+// We can unpack the values we want right in the parameter list:
+function print({ first, last, title }) {
+  console.log(`${first} ${last}, ${title}`);
+}
+
+const response = ["HTTP/1.1", "200 OK", "application/json"];
+
+// Also works with array parameters:
+function parseResponse([protocol, statusCode, contentType]) {
+  console.log(`Status: ${statusCode}`);
+}
+
+parseResponse(response); //Status: 200 OK
