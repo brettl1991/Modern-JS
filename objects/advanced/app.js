a8501619380c1371c989c117a83fd4126922d5ad
brettl1991 <brettl1991@gmail.com>
2022-11-18 14:55:46 +0000

    This keyword

--- objects/advanced/app.js
+++ objects/advanced/app.js
@@ -133,3 +133,15 @@ const auth = {
     console.log("GOODBYE");
   },
 };
+
+//This keyword
+function sayHi() {
+  console.log("HI");
+  //this refers to the window (global scope object in the browser)
+  console.log(this);
+}
+
+const greet = function () {
+  //this refers to the window (global scope object in the browser)
+  console.log(this);
+};
