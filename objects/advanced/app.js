fb4fc2e0411948268030b88fac6f01318cae24b1
brettl1991 <brettl1991@gmail.com>
2022-11-18 14:16:27 +0000

    Computed properties

--- objects/advanced/app.js
+++ objects/advanced/app.js
@@ -62,3 +62,48 @@ function getCard() {
     suit,
   };
 }
+
+const role = "host";
+const person = "Jools Holland";
+const role2 = "Director";
+const person2 = "James Cameron";
+
+// The old way:
+// Make the object
+// const team = {};
+// Then add a property using dynamic key:
+// team[role] = person;
+// team[role2] = person2;
+
+// USING COMPUTED PROPERTIES!
+const team = {
+  [role]: person,
+  [role2]: person2,
+  [1 + 6 + 9]: "sixteen",
+};
+
+//old way
+// function addProp(obj, k, v) {
+//   const copy = {
+//     ...obj
+//   };
+//   copy[k] = v;
+//   return copy;
+// }
+
+// const addProp = (obj, k, v) => {
+//   return {
+//     ...obj,
+//     [k]: v
+//   }
+// }
+
+//refactored:
+const addProp = (obj, k, v) => ({
+  ...obj,
+  [k]: v,
+});
+const res = addProp(team, "happy", ":)");
+console.log(res);
+
+//{16: 'sixteen', host: 'Jools Holland', Director: 'James Cameron', happy: ':)'}
